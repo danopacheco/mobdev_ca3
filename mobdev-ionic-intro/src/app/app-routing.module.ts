8b613bad72e0829737ee33b12bd9432d2f1fbd4c
danopacheco <2022038@student.cct.ie>
2022-11-11 17:06:40 +0000

    modifying mistakes

--- mobdev-ionic-intro/src/app/app-routing.module.ts
+++ mobdev-ionic-intro/src/app/app-routing.module.ts
@@ -2,36 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'characters',
-    loadChildren: () => import('./pages/characters/characters.module').then( m => m.CharactersPageModule)
-  },
-  {
-    path: 'episodes',
-    loadChildren: () => import('./pages/episodes/episodes.module').then( m => m.EpisodesPageModule)
-  },
-  {
-    path: 'quotes',
-    loadChildren: () => import('./pages/quotes/quotes.module').then( m => m.QuotesPageModule)
-  },
+  {  path: '', loadChildren: () => import('./pages/tab/tab.module').then( m => m.TabPageModule)}
 
-  {
-    path: 'death-count',
-    loadChildren: () => import('./pages/death-count/death-count.module').then( m => m.DeathCountPageModule)
-  },
-  {
-    path: 'tab',
-    loadChildren: () => import('./pages/tab/tab.module').then( m => m.TabPageModule)
-  },
 ];
 
 @NgModule({
