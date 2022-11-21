2d79ae340db6ec739d9774a64769ecef740e8737
danopacheco <2022038@student.cct.ie>
2022-11-21 16:34:33 +0000

    adding api service

--- mobdev-ionic-intro/src/app/pages/character-details/character-details.page.ts
+++ mobdev-ionic-intro/src/app/pages/character-details/character-details.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-character-details',
@@ -9,14 +10,17 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CharacterDetailsPage implements OnInit {
   character: any;
+  characterId: string;
 
-  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+  constructor(private activatedRoute: ActivatedRoute, private api: ApiService) { }
 
   ngOnInit() {
-    let id = this.activatedRoute.snapshot.paramMap.get('id');
-    this.http.get(`https://www.breakingbadapi.com/api/characters${id}`).subscribe(res => {
-      this.character = res;
-    });
+   // let id = this.activatedRoute.snapshot.paramMap.get('id');
+    //this.http.get(`https://www.breakingbadapi.com/api/characters${id}`).subscribe(res => {
+      //this.character = res;
+    this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.api.getCharacter(this.characterId).subscribe(res => {this.character = res[0]; });
+  
   }
 
 }
